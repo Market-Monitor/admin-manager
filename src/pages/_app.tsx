cab64446f9f43a846ea22cdf452b2081fcb3cc8c
tbdsux <iamcoderx@gmail.com>
2023-01-30 21:44:35 +0800

    add tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Sofia_Sans } from "@next/font/google";
+import type { AppProps } from "next/app";
+
+const sofiaSans = Sofia_Sans({
+  subsets: ["latin"],
+  variable: "--font-sofia-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${sofiaSans.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sofia-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
